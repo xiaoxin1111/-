57efa839aed7a3e8e697ffb671ca9679122c0bec
xiaoxin1111 <1930667309@qq.com>
2018-08-31 17:04:59 +0800

    Update 雪花特效.js

--- 雪花特效.js
+++ 雪花特效.js
@@ -1,26 +1,29 @@
 function create(){
-    //创建雪花
+    //创建流星
     let el = document.createElement("div");
     el.innerHTML = "*";
     el.style.top = "0px";
     el.style.position = "absolute";
-    el.style.color = "#0FF";
-    el.style.fontSize = "32px";
+    el.style.color = "#FFFAF0";
+    el.style.fontSize = "30px";
     el.style.zIndex = "10000";
+    el.style.transform = "rotate(0deg)"
     return new animate(el); 
   }
   function animate(el){
-    //储存创建好的雪花
+    //储存创建好的流星
     this.el = el;
     //初始位置
     this.direaction();
-    //飘雪花最大高度
+    //判断是往左飞还是往右飞
+    this.dir_center();
+    //飘流星最大高度
     this.maxHeight = this.sumHeight();
-    //添加雪花
+    //添加流星
     document.getElementsByTagName("body")[0].appendChild(this.el);
-    //下雪花
+    //下流星
     this.down_snow();
-    //清除雪花
+    //清除流星
     this.clear_snow();
   }
   animate.prototype = {
@@ -28,30 +31,59 @@ function create(){
       this.el.style.left = Math.random()*document.documentElement.clientWidth - 100 + "px";
     },
     sumHeight(){
-      return screen.height;
+      return document.getElementsByTagName("body")[0].offsetHeight - 50;
+    },
+    dir_center(){
+      this.center = document.documentElement.clientWidth / 2;
+      let left = parseInt(this.el.style.left.replace("px",""));
+      if(left > this.center){
+        this.move = 0;
+        return false;
+      }
+      this.move = 1;
     },
     down_snow(){
       this.timer = setInterval(()=>{
         let top = getComputedStyle(this.el,null)["top"].replace("px","");
-        top = parseInt(top) + 2;
         let left = getComputedStyle(this.el,null)["left"].replace("px","");
-        left = parseInt(left) - 1;
+        if(this.move == 0){
+          top = parseInt(top) + 2;
+          left = parseInt(left) - 1;
+          this.maxHeight -= 2;
+        }else{
+          top = parseInt(top) + 2;
+          left = parseInt(left) + 1;
+          this.maxHeight -= 2;
+        }
+        this.tletf = this.tletf == undefined?0:this.tletf;
+        //旋转
+        this.tletf = this.tletf == undefined?0:this.tletf+=10;
+        this.el.style.transform = `rotate(${this.tletf}deg)`
         this.el.style.top = top + "px";
         this.el.style.left = left + "px";
-        this.maxHeight -= 2;
       },12)
     },
     clear_snow(){
       let time = setInterval(()=>{
         if(this.maxHeight <= 0){
-          clearInterval(this.time);
-          clearInterval(this.timer);
-          this.el.remove();
+          //重新定位Y轴
+          this.el.style.top = 0;
+          //判断是否在左侧
+          this.direaction();
+            //重新定位高度
+            this.maxHeight = this.sumHeight();
+          //判断是往左飞还是往右飞
+          this.dir_center();
         }
       },12)
     }
   }
-  //设置雪花
-  setInterval(function(){
-    create();
-  },200)
+  //设置星星
+  let time = setInterval(function(){
+    if(i<50){
+      i++;
+      create();
+      return false;
+    }
+    clearInterval(time);
+  },400)
